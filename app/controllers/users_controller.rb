dbf4d656d73f00996b2751b94d924800b6d355cc
Jair Jaramillo <jair.jaramillo@outlook.com>
2019-12-23 10:51:05 -0600

    Adding SignUp controller and viewer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(email: params[:email], name: params[:name])
+
+    if @user.save
+      flash[:success] = "Success Sign Up"
+      redirect_to @user
+    else
+      flash.now[:error] = "There is a problem"
+    end
   end
 
   def show
